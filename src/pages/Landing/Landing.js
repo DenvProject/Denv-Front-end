9b82942705ab269887daee060a57777bfa9f28ce
Rafaelltm <rafaelltm10@hotmail.com>
2021-03-16 17:16:12 -0300

    Continue the develompment of the landingPage
    Co-authored-by: abner423<abner.f.c.r@hotmail.com>

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -13,8 +13,8 @@ export default class LandingPage extends React.Component {
         <nav class="navbar"></nav>
         <div id="main" class="main">
           <div class="logo">
-            <p>DENV</p>
-            <img 
+            <h1>DENV</h1>
+            <img
               src={Logo}
               alt="Logo do denv"
             />
@@ -23,20 +23,58 @@ export default class LandingPage extends React.Component {
           <button class="searchBtn">Pesquisar</button>
 
           <a class="arrowDown" href="#about">
-            <img 
+            <img
               src={SetaParaBaixo}
               alt="Botão para levar a tela à ancora sobre"
             />
           </a>
         </div>
         <div id="about" class="about">
+          <div class="divTitle">
+            <h2>Sobre</h2>
+          </div>
+
+          <div class="hiwCards">
+            <div className="hiwCard">
+              <span className="hiwTitle">
+                <h3>Lorem Ipsum</h3>
+              </span>
+              <span className="hiwContent">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
+                Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
+              </span>
+            </div>
+
+            <div className="hiwCard">
+              <span className="hiwTitle">
+                <h3>Lorem Ipsum</h3>
+              </span>
+              <span className="hiwContent">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
+                Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
+              </span>
+            </div>
+
+            <div className="hiwCard">
+              <span className="hiwTitle">
+                <h3>Lorem Ipsum</h3>
+              </span>
+              <span className="hiwContent">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
+                Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
+              </span>
+            </div>
+          </div>
 
         </div>
         <div id="hiw" class="hiw">
 
         </div>
         <div id="faq" class="faq">
-          
+
         </div>
       </div>
     );
