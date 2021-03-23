6cd5b36a484a910f30f53458e9fdd16a9f18b945
Rafaelltm <rafaelltm10@hotmail.com>
2021-03-23 02:24:38 -0300

    add faq to LandingPage and fixed some class names
    Co-authored-by: abner423<abner.f.c.r@hotmail.com>

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,10 +3,8 @@ import React from 'react';
 import './styles.css';
 import Logo from '../../assets/images/Logo.svg';
 import SetaParaBaixo from '../../assets/images/SetaParaBaixo.svg';
-import Graphics1 from '../../assets/images/graphics.jpg';
-import Graphics2 from '../../assets/images/graphics2.jpg';
-
-import { Link } from 'react-router-dom';
+import LogoSUS from '../../assets/images/Logo_SUS.png';
+import Graphics from '../../assets/images/graphics2.jpg';
 
 export default class LandingPage extends React.Component {
   render() {
@@ -36,34 +34,34 @@ export default class LandingPage extends React.Component {
             <h2>Sobre</h2>
           </div>
 
-          <div class="hiwCards">
-            <div className="hiwCard">
-              <span className="hiwTitle">
+          <div class="aboutCards">
+            <div className="aboutCard">
+              <span className="aboutTitle">
                 <h3>Lorem Ipsum</h3>
               </span>
-              <span className="hiwContent">
+              <span className="aboutContent">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
                 Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
               </span>
             </div>
 
-            <div className="hiwCard">
-              <span className="hiwTitle">
+            <div className="aboutCard">
+              <span className="aboutTitle">
                 <h3>Lorem Ipsum</h3>
               </span>
-              <span className="hiwContent">
+              <span className="aboutContent">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
                 Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
               </span>
             </div>
 
-            <div className="hiwCard">
-              <span className="hiwTitle">
+            <div className="aboutCard">
+              <span className="aboutTitle">
                 <h3>Lorem Ipsum</h3>
               </span>
-              <span className="hiwContent">
+              <span className="aboutContent">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
                 Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
@@ -76,33 +74,59 @@ export default class LandingPage extends React.Component {
           <div class="divTitle">
             <h2>Como Funciona</h2>
           </div>
-          
+
           <div class="hiwParts pt1">
-            <img src={Graphics1} alt="exemplos de gráficos"/>
+            <img src={LogoSUS} alt="exemplos de gráficos" />
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-              labore et dolore magna aliqua. Nulla facilisi cras fermentum odio eu. Arcu dui vivamus arcu felis 
-              bibendum ut tristique. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris. 
-              Purus ut faucibus pulvinar elementum integer enim neque. Rhoncus urna neque viverra justo nec 
-              ultrices. In fermentum et sollicitudin ac orci phasellus egestas. Morbi quis commodo odio aenean 
-              sed adipiscing diam donec. Nisi vitae suscipit tellus mauris.
+              Os dados são coletados apartir da api do próprio SUS. O DataSUS.
             </p>
           </div>
 
           <div class="hiwParts pt2">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-              labore et dolore magna aliqua. Nulla facilisi cras fermentum odio eu. Arcu dui vivamus arcu felis 
-              bibendum ut tristique. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris. 
-              Purus ut faucibus pulvinar elementum integer enim neque. Rhoncus urna neque viverra justo nec 
-              ultrices. In fermentum et sollicitudin ac orci phasellus egestas. Morbi quis commodo odio aenean 
-              sed adipiscing diam donec. Nisi vitae suscipit tellus mauris.
+              Depois de processados, os dados são disponibilizados em forma de gráficos.
+              A fim de manter a sociedade o mais informada possível.
             </p>
-            <img src={Graphics2} alt="exemplos de gráficos" />
+            <img src={Graphics} alt="exemplos de gráficos" />
           </div>
         </div>
         <div id="faq" class="faq">
+          <div class="divTitle">
+            <h2>FAQ</h2>
+          </div>
 
+          <div class="faqCards">
+            <div className="faqCard">
+              <span className="faqQuestion">
+                <h3>Preciso pagar para usar o DENV?</h3>
+              </span>
+              <span className="faqContent">
+                Não, o DENV é uma plataforma gratuita que tem como objetivo manter a população
+                o mais informada possível sobre a situação de determinadas doenças.  
+              </span>
+            </div>
+
+            <div className="faqCard">
+              <span className="faqQuestion">
+                <h3>Como faço para obter informações sobre alguma doença?</h3>
+              </span>
+              <span className="faqContent">
+                Para acessar os gráficos sobre alguma doença basta no início dessa página, clicar em "Pesquisar",
+                Digitar o nome da doença em questão e selecionar os filtros de pesquisa desejados.  
+              </span>
+            </div>
+
+            <div className="faqCard">
+              <span className="faqQuestion">
+                <h3>Lorem Ipsum</h3>
+              </span>
+              <span className="faqContent">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui.
+                Pulvinar pellentesque habitant morbi tristique senectus et. Dolor sit amet consectetur adipiscing elit duis.
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     );
